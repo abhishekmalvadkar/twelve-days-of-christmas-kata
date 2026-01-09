bd5faa986ca70d7da96716b9d4596de27ad86a43
Abhishek <abhi@dev.com>
2026-01-09 11:54:33 +0530

    Initial commit

---
+++ src/main/java/com/amalvadkar/tdock/TwelveDaysOfChristmasKataApplication.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.tdock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwelveDaysOfChristmasKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwelveDaysOfChristmasKataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amalvadkar/tdock/TwelveDaysOfChristmasKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.tdock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwelveDaysOfChristmasKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
