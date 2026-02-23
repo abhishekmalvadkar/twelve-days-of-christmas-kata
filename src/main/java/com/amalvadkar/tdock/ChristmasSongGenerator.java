28f2d0809534e9392a9db8cc28c2a1d6772bb431
Abhishek <abhi@dev.com>
2026-02-23 12:05:46 +0530

    Scenario:     Singing the song on the first day
    Given
        it is the first day of Christmas
    When
        I sing the Christmas song
    Then
        the verse should be:
        On the first day of Christmas
        My true love gave to me:
        A partridge in a pear tree.

---
+++ src/main/java/com/amalvadkar/tdock/ChristmasSongGenerator.java
@@ -0,0 +1,11 @@
+package com.amalvadkar.tdock;
+
+public class ChristmasSongGenerator {
+
+    public String generate(ChristmasDay christmasDay) {
+        return """
+                On the first day of Christmas
+                My true love gave to me:
+                A partridge in a pear tree.""";
+    }
+}
